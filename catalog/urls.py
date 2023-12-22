f06a110ded1d8a297126abb95daec9bdb9b14f05
Arina <arinafedor23@icloud.com>
2023-12-23 01:43:50 +0300

    add new files

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,9 +1,16 @@
-from django.urls import path
+from django.urls import path, include
 
-from catalog.views import contacts, home
+from catalog.apps import CatalogConfig
+from catalog.views import contacts, home, products, index, category, category_catalog
+
+app_name = CatalogConfig.name
 
 
 urlpatterns = [
-    path('contacts/', contacts),
-    path('home/', home),
+    path('contacts/', contacts, name='contacts'),
+    path('home/', home, name='home'),
+    path('products/', products, name='products'),
+    path('index/', index, name='index'),
+    path ('category/', category, name='category'),
+    path ('<int:pk>/catalog/', category_catalog, name='category_catalog'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from catalog.models import Product, Category
+
 
 def contacts(request):
     if request.method == 'POST':
@@ -12,3 +14,26 @@ def contacts(request):
 
 def home(request):
     return render(request, 'catalog/home.html')
+
+
+def products(request):
+    context = {'object_list': Product.objects.all(),
+               'title': 'Продукты - Наши товары'}
+    return render(request, 'catalog/products.html', context)
+
+
+def index(request):
+    context = {'object_list': Category.objects.all(),
+               'title': 'Продукты - Наши категории'}
+    return render(request, 'catalog/index.html', context)
+
+def category(request):
+    context = {'object_list': Category.objects.all(),
+               'title': 'Продукты - Наши товары'}
+    return render(request, 'catalog/category.html', context)
+
+def category_catalog(request, pk):
+    category_item = Category.objects.get(pk=pk)
+    context = {'object_list': Product.objects.filter(category_id=pk),
+               'title': f'Наши товары по категориям {category_item.name}'}
+    return render(request, 'catalog/products.html', context)
